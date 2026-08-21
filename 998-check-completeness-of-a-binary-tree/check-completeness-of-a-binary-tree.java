78aec50c64fc73a5d2b84539dc93bb4c5d8f7d97
Sankalp Omar <sankalpomar6115@gmail.com>
2026-08-21 21:15:37 +0530

    Time: 1 ms (83.06%) | Memory: 43.8 MB (76.38%) - LeetSync

--- 998-check-completeness-of-a-binary-tree/check-completeness-of-a-binary-tree.java
+++ 998-check-completeness-of-a-binary-tree/check-completeness-of-a-binary-tree.java
@@ -1,35 +1,26 @@
 class Solution {
-    public List<String> bfs(TreeNode root){
-        List<String> list = new ArrayList<>();
-        if(root == null) return list;
-        
+    public boolean isCompleteTree(TreeNode root) {
         Queue<TreeNode> q = new LinkedList<>();
         q.offer(root);
 
-        while(!q.isEmpty()){
-            TreeNode peek = q.poll();
-            if(peek == null){
-                list.add("null");
+        boolean seenNull = false;
+
+        while (!q.isEmpty()) {
+            TreeNode curr = q.poll();
+
+            if (curr == null) {
+                seenNull = true;
                 continue;
             }
-            list.add(String.valueOf(peek.val));
-            q.offer(peek.left);
-            q.offer(peek.right);
-        }
-        while (!list.isEmpty() && list.get(list.size() - 1).equals("null")) {
-            list.remove(list.size() - 1);
-        }
 
-        return list;
-    }
-    public boolean isCompleteTree(TreeNode root) {
-        List<String> list = bfs(root);
-        System.out.println(list);
-        for(int i=0;i<list.size();i++){
-            if(i != list.size()-1 && list.get(i) == "null"){
+            if (seenNull) {
                 return false;
             }
+
+            q.offer(curr.left);
+            q.offer(curr.right);
         }
+
         return true;
     }
 }
