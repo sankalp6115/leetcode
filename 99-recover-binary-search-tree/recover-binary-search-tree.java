5e7a5c29699f6ba5fdc904223ad18ce815837d4a
Sankalp Omar <sankalpomar6115@gmail.com>
2026-08-22 12:15:30 +0530

    Time: 25 ms (1.42%) | Memory: 47.2 MB (12.52%) - LeetSync

---
+++ 99-recover-binary-search-tree/recover-binary-search-tree.java
@@ -0,0 +1,42 @@
+class Solution {
+    public void inorder(TreeNode root,List<Integer> normalList,List<Integer> sortedList){
+        if(root == null) return;
+        inorder(root.left,normalList,sortedList);
+        normalList.add(root.val);
+        sortedList.add(root.val);
+        inorder(root.right,normalList,sortedList);
+    }
+    public void replace(TreeNode root,int diff1,int diff2){
+        if(root == null) return;
+        replace(root.left,diff1,diff2);
+        if(root.val == diff2){
+            root.val = diff1;
+        }
+        else if(root.val == diff1){
+            root.val = diff2;
+        }
+        replace(root.right,diff1,diff2);
+    }
+    public void recoverTree(TreeNode root) {
+        List<Integer> normal = new ArrayList<>();
+        List<Integer> sorted = new ArrayList<>();
+        inorder(root,normal,sorted);
+        Collections.sort(sorted);
+        System.out.println(normal);
+        System.out.println(sorted);
+
+        int diff1=0;
+        int diff2=0;
+        for(int i=0;i<normal.size();i++){
+            if(!normal.get(i).equals(sorted.get(i))){
+                diff1 = normal.get(i);
+                diff2 = sorted.get(i);
+                break;
+            }
+        }
+        System.out.println(diff1);
+        System.out.println(diff2);
+
+        replace(root,diff1,diff2);
+    }
+}
