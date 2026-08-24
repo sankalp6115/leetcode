83f1e26d81caf26eef53e169cdbbbd5d1cf10308
Sankalp Omar <sankalpomar6115@gmail.com>
2026-08-24 15:12:43 +0530

    Time: 60 ms (5.81%) | Memory: 48.1 MB (23.33%) - LeetSync

--- 658-find-k-closest-elements/find-k-closest-elements.java
+++ 658-find-k-closest-elements/find-k-closest-elements.java
@@ -1,42 +1,47 @@
 class Solution {
-    class Pair{
+    class Pair {
         int val;
         int diff;
-        int freq;
-        Pair(int val,int diff,int freq){
+
+        Pair(int val, int diff) {
             this.val = val;
             this.diff = diff;
-            this.freq = freq;
         }
     }
+
     public List<Integer> findClosestElements(int[] arr, int k, int x) {
         HashMap<Integer, Integer> map = new HashMap<>();
-        for(int i=0;i<arr.length;i++){
-            map.put(arr[i],map.getOrDefault(arr[i],0)+1);
+
+        for (int val : arr) {
+            map.put(val, map.getOrDefault(val, 0) + 1);
         }
 
-        PriorityQueue<Pair> pq = new PriorityQueue<>((a,b) -> {
-            if(a.diff == b.diff){
-                return Integer.compare(b.val,a.val);
+        PriorityQueue<Pair> pq = new PriorityQueue<>((a, b) -> {
+            if (a.diff == b.diff) {
+                return Integer.compare(b.val, a.val);
             }
-            else{
-                return Integer.compare(b.diff,a.diff);
-            }
+            return Integer.compare(b.diff, a.diff);
         });
 
-        for(Map.Entry<Integer,Integer> entry:map.entrySet()){
-            Pair pair = new Pair(entry.getKey(),Math.abs(x - entry.getKey()),entry.getValue());
-            for(int i=0;i<entry.getValue();i++){
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            Pair pair = new Pair(
+                entry.getKey(),
+                Math.abs(x - entry.getKey())
+            );
+
+            for (int i = 0; i < entry.getValue(); i++) {
                 pq.offer(pair);
             }
-            while(pq.size() > k){
+
+            while (pq.size() > k) {
                 pq.poll();
             }
         }
 
         List<Integer> ans = new ArrayList<>();
-        for(Pair p:pq){
-            ans.add(p.val);
+
+        while (!pq.isEmpty()) {
+            ans.add(pq.poll().val);
         }
 
         Collections.sort(ans);
