ad22f0535483cfb0203a7f7b990513b266aa82a4
Sankalp Omar <sankalpomar6115@gmail.com>
2026-09-19 14:01:32 +0530

    Time: 9 ms (34.09%) | Memory: 49 MB (74.83%) - LeetSync

--- 56-merge-intervals/merge-intervals.java
+++ 56-merge-intervals/merge-intervals.java
@@ -1,24 +1,29 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
-        Arrays.sort(intervals, (a,b) -> Integer.compare(a[0],b[0]));
-        int n = intervals.length;
-        int[][] ans = new int[n][2];
-        int idx=0;
-        int start = intervals[0][0];
-        int end = intervals[0][1];
-        
+        Arrays.sort(intervals, (row1,row2) -> Integer.compare(row1[0],row2[0]));
+        int start1 = intervals[0][0];
+        int end1 = intervals[0][1];
+        List<List<Integer>> ans = new ArrayList<>();
         for(int i=1;i<intervals.length;i++){
-            if(intervals[i][0] <= end){
-                end = Math.max(end,intervals[i][1]);
-            }
-            else{
-                ans[idx++] = new int[]{start,end};
-                start = intervals[i][0];
-                end = intervals[i][1];
-            }
-        }
+            int start2 = intervals[i][0];
+            int end2 = intervals[i][1];
 
-        ans[idx++] = new int[]{start,end};
-        return Arrays.copyOf(ans, idx);
+            if(end1 >= start2){
+                start1 = start1;
+                end1 = Math.max(end1,end2);
+                continue;
+            }
+
+            ans.add(Arrays.asList(start1,end1));
+            start1 = start2;
+            end1 = end2;
+        }
+        ans.add(Arrays.asList(start1,end1));
+        int[][] ansArray = new int[ans.size()][2];
+        for(int i=0;i<ans.size();i++){
+            int[] arr = new int[]{ans.get(i).get(0),ans.get(i).get(1)};
+            ansArray[i] = arr;
+        }
+        return ansArray;
     }
 }
