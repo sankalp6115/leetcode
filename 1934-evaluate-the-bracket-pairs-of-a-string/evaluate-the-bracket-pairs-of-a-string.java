a14026fdc404841a02002007aa7c10b2de615b80
Sankalp Omar <sankalpomar6115@gmail.com>
2026-09-26 19:03:32 +0530

    Time: 52 ms (14.03%) | Memory: 100.1 MB (7.69%) - LeetSync

---
+++ 1934-evaluate-the-bracket-pairs-of-a-string/evaluate-the-bracket-pairs-of-a-string.java
@@ -0,0 +1,37 @@
+class Solution {
+    public String evaluate(String s, List<List<String>> knowledge) {
+        HashMap<String,String> map = new HashMap<>();
+        for(int i=0;i<knowledge.size();i++){
+            String key = knowledge.get(i).get(0);
+            String value = knowledge.get(i).get(1);
+            map.put(key,value);
+        }
+
+        StringBuilder sb = new StringBuilder();
+        Queue<Character> st = new LinkedList<>();
+        boolean putIntoStack = false;
+        for(int i=0;i<s.length();i++){
+            char curr = s.charAt(i);
+            if(curr == '('){
+                putIntoStack = true;
+            }
+            else if(s.charAt(i) == ')'){
+                putIntoStack = false;
+                StringBuilder word = new StringBuilder();
+                while(!st.isEmpty()){
+                    word.append(st.poll());
+                }
+                sb.append(map.getOrDefault(word.toString(),"?"));
+            }
+            else{
+                if(putIntoStack){
+                    st.offer(curr);
+                }
+                else{
+                    sb.append(curr);
+                }
+            }
+        }
+        return sb.toString();
+    }
+}
