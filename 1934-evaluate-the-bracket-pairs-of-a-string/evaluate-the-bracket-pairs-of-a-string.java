83b25950b9cefb850a087bced8302e6550cdc626
Sankalp Omar <sankalpomar6115@gmail.com>
2026-09-26 19:04:12 +0530

    Time: 63 ms (11.31%) | Memory: 97.2 MB (20.81%) - LeetSync

--- 1934-evaluate-the-bracket-pairs-of-a-string/evaluate-the-bracket-pairs-of-a-string.java
+++ 1934-evaluate-the-bracket-pairs-of-a-string/evaluate-the-bracket-pairs-of-a-string.java
@@ -8,7 +8,7 @@ class Solution {
         }
 
         StringBuilder sb = new StringBuilder();
-        Queue<Character> st = new LinkedList<>();
+        Queue<Character> st = new ArrayDeque<>();
         boolean putIntoStack = false;
         for(int i=0;i<s.length();i++){
             char curr = s.charAt(i);
